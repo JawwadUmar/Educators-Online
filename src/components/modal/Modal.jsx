4c96b112ca832b284bd3634fa18d0155773f71f7
Jawwad <jawwadumar99@gmail.com>
2023-12-17 03:54:36 +0530

    modal created successfully

--- src/components/modal/Modal.jsx
+++ src/components/modal/Modal.jsx
@@ -1,24 +1,40 @@
-import React from 'react'
-import './Modal.css'
+import React from 'react';
+import './Modal.css';
+import ReactDOM from 'react-dom';
 
-const Modal = ({open, close, title, message}) => {
-  if(!open){
+const Modal = ({ open, close, title, message }) => {
+  
+  React.useEffect(() => {
+    if (open) {
+      document.body.classList.add('modal-open');
+    } else {
+      document.body.classList.remove('modal-open');
+    }
+
+    return () => {
+      document.body.classList.remove('modal-open');
+    };
+  }, [open]);
+
+  if (!open) {
     return null;
   }
 
-  return (
+  return ReactDOM.createPortal(
     <>
-    <div className="modal">
-    <div className="modal-overlay" onClick={close}></div>
-    <div className="modal-content">
-    <h2>{title}</h2>
-    <p>{message}</p>
-    <button className='close-modal'
-    onClick={close}>Ok</button>
-    </div>
-    </div>
-    </>
-  )
-}
+      <div className="modal">
+        <div className="modal-overlay" onClick={close}></div>
+        <div className="modal-content">
+          <h2>{title}</h2>
+          <p>{message}</p>
+          <button className="close-modal" onClick={close}>
+            Ok
+          </button>
+        </div>
+      </div>
+    </>,
+    document.getElementById('portal')
+  );
+};
 
-export default Modal
+export default Modal;
